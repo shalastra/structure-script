19a3191c593e1095e31b1ca75820528d9f76b425
Szymon Halastra <szymon.halastra@gmail.com>
2017-12-14 17:54:54 +0100

    Fix input

--- script.sh
+++ script.sh
@@ -79,16 +79,22 @@ done
 while [[ "${varpackage}" = "" ]]; do
     read -p 'package name(domain): ' varpackage
 
-    if [[ "${varpackage}" =~ "^(([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|([a-zA-Z0-9][a-zA-Z0-9-_]{1,61}[a-zA-Z0-9]))\.([a-zA-Z]{2,6}|[a-zA-Z0-9-]{2,30}\.[a-zA-Z]{2,3})$" ]];
+    if [[ "$varpackage" != *"."* ]];
     then
-        echo "Correct"
-    else
-        echo "Incorrect domain, try again."
+        echo "Incorrect domain name, should be in format: xxx.xxx.xxx, try again."
         varpackage=""
     fi
 done
 
-read -p 'version(default: 0.0.1): ' varversion
+while [[ "${varversion}" = "" ]]; do
+    read -p 'version(default: 0.0.1): ' varversion
+
+    if [[ "$varversion" != *"."* ]];
+    then
+        echo "Incorrect version format, should be xxx.xxx.xxx, try again."
+        varversion=""
+    fi
+done
 
 echo "Entered data, please verify:"
 echo "Project name: " $varname
